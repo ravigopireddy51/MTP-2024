abfa794794c7a7604b504f47222ee32a59ac3ef2
ravindra <root@ip-172-31-9-174.ap-south-1.compute.internal>
2019-11-24 07:10:28 +0000

    priya added

--- cc.java
+++ cc.java
@@ -2,15 +2,4 @@ echo  "enter a name:
 read name
 echo "HI $name,Welcome"
 
-<<<<<<< HEAD
-  echo "welcome to Hyderabad"
-
-shankar
-Indian Team
-
-
-
-
-=======
-  echo "welcome to Nellore"
->>>>>>> 0341559... Welcome Nellore Added
+ Welcome to priyadarshini
