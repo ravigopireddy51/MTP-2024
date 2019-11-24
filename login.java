6ad51e5dc3e4f2fb0e7f6a61417f4f0c37602a74
ravindra <root@ip-172-31-9-174.ap-south-1.compute.internal>
2019-11-24 06:26:29 +0000

    Login Button Added

--- login.java
+++ login.java
@@ -2,14 +2,5 @@ read -p "enter a Name"
 read name
 echo "HI $name, Welcome"
 
+Login Buttin
 
-Today is Thursday
-
-tomorrow is friday
-
-day after tomorrow is saturday
-
-This is the 12th class of Devops
-
-
-This is the 20th class of Devops
